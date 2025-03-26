8ade5b807027ad865e0598c4318b432e002c1da6
blaxovios <tsepe21@hotmail.com>
2025-03-27 01:28:58 +0200

    Need to accept consent and scrape again from start_url

--- yahoo_finance_news_spider/settings.py
+++ yahoo_finance_news_spider/settings.py
@@ -12,6 +12,10 @@ BOT_NAME = "yahoo_finance_news_spider"
 SPIDER_MODULES = ["yahoo_finance_news_spider.spiders"]
 NEWSPIDER_MODULE = "yahoo_finance_news_spider.spiders"
 
+RETRY_ENABLED = True
+RETRY_TIMES = 2
+RETRY_HTTP_CODES = [500, 502, 503, 504, 522, 524, 408, 429, 400, 403, 404]
+LOG_LEVEL='DEBUG'
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = "yahoo_finance_news_spider (+http://www.yourdomain.com)"
@@ -32,16 +36,18 @@ RANDOMIZE_DOWNLOAD_DELAY = True
 #CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
-#COOKIES_ENABLED = False
+COOKIES_ENABLED = False
 
 # Disable Telnet Console (enabled by default)
-#TELNETCONSOLE_ENABLED = False
+TELNETCONSOLE_ENABLED = False
 
 # Override the default request headers:
-#DEFAULT_REQUEST_HEADERS = {
-#    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-#    "Accept-Language": "en",
-#}
+DEFAULT_REQUEST_HEADERS = {
+  'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+  'Accept-Language': 'en-US,en;q=0.5',
+  'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+  'Referer': 'https://finance.yahoo.com/',
+}
 
 # Enable or disable spider middlewares
 # See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
@@ -54,6 +60,10 @@ SPIDER_MIDDLEWARES = {
 #DOWNLOADER_MIDDLEWARES = {
 #    "yahoo_finance_news_spider.middlewares.YahooFinanceNewsSpiderDownloaderMiddleware": 543,
 #}
+DOWNLOADER_MIDDLEWARES = {
+    'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,
+    'scrapy_user_agents.middlewares.RandomUserAgentMiddleware': 400,
+}
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
@@ -63,20 +73,20 @@ SPIDER_MIDDLEWARES = {
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    "yahoo_finance_news_spider.pipelines.YahooFinanceNewsSpiderPipeline": 300,
-#}
+ITEM_PIPELINES = {
+   "yahoo_finance_news_spider.pipelines.YahooFinanceNewsSpiderPipeline": 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
-#AUTOTHROTTLE_ENABLED = True
+AUTOTHROTTLE_ENABLED = True
 # The initial download delay
 #AUTOTHROTTLE_START_DELAY = 5
 # The maximum download delay to be set in case of high latencies
 #AUTOTHROTTLE_MAX_DELAY = 60
 # The average number of requests Scrapy should be sending in parallel to
 # each remote server
-#AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
+AUTOTHROTTLE_TARGET_CONCURRENCY = CONCURRENT_REQUESTS
 # Enable showing throttling stats for every response received:
 #AUTOTHROTTLE_DEBUG = False
 
@@ -90,4 +100,14 @@ SPIDER_MIDDLEWARES = {
 
 # Set settings whose default value is deprecated to a future-proof value
 TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
-FEED_EXPORT_ENCODING = "utf-8"
+FEEDS = {
+    'scraped_data.json': {
+        'format': 'json',
+        'encoding': 'utf-8',
+    }
+}
+
+PLAYWRIGHT_BROWSER_TYPE = 'chromium'
+PLAYWRIGHT_LAUNCH_OPTIONS = {
+    'headless': False,
+}
--- yahoo_finance_news_spider/spiders/yahoo_finance_news_spider.py
+++ yahoo_finance_news_spider/spiders/yahoo_finance_news_spider.py
@@ -2,6 +2,7 @@ from scrapy.loader import ItemLoader
 from scrapy.spiders import Spider
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import Rule
+from scrapy_playwright.page import PageMethod
 from scrapy.http import Request
 from datetime import datetime
 import uuid
@@ -10,36 +11,70 @@ from yahoo_finance_news_spider.items import YahooFinanceNewsSpiderItem
 
 class YahooFinanceNewsSpider(Spider):
     name = "yahoofinance_news"
+    
+    start_urls = [
+        'https://finance.yahoo.com/',
+    ]
     dont_follow = [
         r'https://consent\.yahoo\.com/v2/.*',
+        r'https://consent\.yahoo\.com/v2/partners\?sessionId.*',
+        r'https://legal\.yahoo\.com/ie/el/yahoo/privacy/index\.html',
     ]
     rules = [
         Rule(LinkExtractor(deny=r'https://consent\.yahoo\.com/v2/.*')),
     ]
     
     def start_requests(self):
-        yield Request('https://finance.yahoo.com/news/', meta={'dont_redirect': True})
+        yield Request(
+        url=self.start_urls[0],
+        # dont_filter=True,
+        callback=self.parse,
+        meta={
+            "dont_proxy": True,  # Disable scrapy-zyte-smartproxy, playwright has separate proxy
+            # "dont_redirect": True,
+            # "handle_httpstatus_list": [302],  # Disables the redirect middleware for the download
+            "playwright": True,  # Use Playwright to execute Javascript
+            "playwright_include_page": True,  # The Playwright page that was used to download the request; available in the callback via response.meta['playwright_page'].
+            "playwright_context": "default",
+            "playwright_page_methods": [
+                PageMethod("wait_for_selector", "button[name='agree']", timeout=60000),  # Wait for the consent button to appear
+                PageMethod("click", "button[name='agree']", timeout=60000),  # Click the consent button
+            ],
+            "playwright_redirect": True,  # Enable redirect handling
+            "playwright_retries": 0,  # Reset retries to 0
+        }
+    )
 
     def parse(self, response):
-        # Extract all links on the page
-        links = response.css('a::attr(href)').get()
-        for link in links:
-            yield response.follow(link, self.parse_link)
+        # If the consent button was clicked, proceed with the original start_url
+        if response.meta.get("playwright_retries") > 0:
+            yield Request(
+                url=self.start_urls[0],
+                callback=self.parse,
+                meta={
+                    "dont_proxy": True,
+                    "playwright": True,
+                    "playwright_include_page": True,
+                    "playwright_context": "default",
+                    "playwright_retries": 0,  # Reset retries to 0
+                }
+            )
+        else:
+            links = response.css('a::attr(href)').getall()
+            for link in links:
+                yield response.follow(link, self.parse_link)
 
     def parse_link(self, response):
-        # Extract data from the crawled pages
         item_loader = ItemLoader(item=YahooFinanceNewsSpiderItem(), response=response)
         item_loader.add_xpath('title', '//div[@class="cover-title"]/text()')
         item_loader.add_xpath('content', '//p[@class="yf-1pe5jgt"]/text()')
         item_loader.add_value('url', response.url)
         item_loader.add_xpath('article_date', '//time/@datetime')
-        item_loader.add_value('timestamp', datetime.datetime.now().isoformat())
+        item_loader.add_value('timestamp', datetime.now().isoformat())
         item = item_loader.load_item()
 
-        # Generate unique ID
         unique_id = str(uuid.uuid5(uuid.NAMESPACE_DNS, response.url))
 
-        # Save scraped data in correct format
         scraped_data = {
             'id': unique_id,
             'url': item['url'],

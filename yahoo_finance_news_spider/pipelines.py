6828b5bd79c80bb116cf71fe71ab801204eca24c
blaxovios <tsepe21@hotmail.com>
2025-04-05 02:38:37 +0300

    Sink to parquet with polars

--- yahoo_finance_news_spider/pipelines.py
+++ yahoo_finance_news_spider/pipelines.py
@@ -1,17 +1,19 @@
-import polars as pl
 import os
-
+import polars as pl
+import datetime
 
 class YahooFinanceNewsSpiderPipeline:
     def __init__(self):
-        self.file_path = 'scraped_data.parquet'
+        self.file_path = 'data/parquet/'
         self.items = []
+        self.file_count = 0
 
     def process_item(self, item, spider):
         self.items.append(dict(item))
-        if len(self.items) >= 10:
+        if len(self.items) >= 100:
             self._append_to_parquet()
             self.items = []
+            self.file_count += 1
         return item
 
     def close_spider(self, spider):
@@ -19,10 +21,9 @@ class YahooFinanceNewsSpiderPipeline:
             self._append_to_parquet()
 
     def _append_to_parquet(self):
-        df = pl.DataFrame(self.items)
-        if not os.path.exists(self.file_path):
-            df.write_parquet(self.file_path)
-        else:
-            existing_df = pl.read_parquet(self.file_path)
-            df = pl.concat([existing_df, df])
-            df.write_parquet(self.file_path)
+        lf = pl.LazyFrame(self.items)
+        timestamp = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
+        file_name = f'scraped_data_{self.file_count:03d}_{timestamp}.parquet'
+        file_path = os.path.join(self.file_path, file_name)
+        os.makedirs(self.file_path, exist_ok=True)
+        lf.sink_parquet(file_path)
